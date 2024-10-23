38b6af4bc43134acbcff302385e7960ab5d83149
Faiza <faizamajid876@gmail.com>
2024-10-23 20:56:09 +0500

    javascript

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-let gameNum=25;
-let userNum=prompt("Guess the game number");
-console.log(userNum);
-while(userNum!=gameNum){
-  userNum=prompt("You entered the wrong number.Guess again ");
-}
-console.log("Conratulation ,you entered the right number")
+// let gameNum=25;
+// let userNum=prompt("Guess the game number");
+// console.log(userNum);
+// while(userNum!=gameNum){
+//   userNum=prompt("You entered the wrong number.Guess again ");
+// }
+// console.log("Conratulation ,you entered the right number")
+
+// Strings
+
+let fullName = prompt("enter your fullname without spaces");
+let userName= "@"+fullName + fullName.length;
+console.log(userName);
+
