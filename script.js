dd077ae5a52e051f2414a69100dab74e8ef0a930
Faiza <faizamajid876@gmail.com>
2024-10-25 16:08:00 +0500

    arrays

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// // let gameNum=25;
-// // let userNum=prompt("Guess the game number");
-// // console.log(userNum);
-// // while(userNum!=gameNum){
-// //   userNum=prompt("You entered the wrong number.Guess again ");
-// // }
-// // console.log("Conratulation ,you entered the right number")
+// let gameNum=25;
+// let userNum=prompt("Guess the game number");
+// console.log(userNum);
+// while(userNum!=gameNum){
+//   userNum=prompt("You entered the wrong number.Guess again ");
+// }
+// console.log("Conratulation ,you entered the right number")
 
 // // Strings
 // let fullName = prompt("enter your fullname without spaces");
@@ -39,12 +39,66 @@
 // let avg=sum/marks.length;
 // console.log(`The average of marks i= ${avg}`);
 // //  Practise Question 2(offer 10%off and change the array to store final price after applying offer)
-// In this query we use "for loop" bcz it give direct value but we ask about index tracking so we use only"for loop";
-let items = [250,645,300,900,50];
-let i=0;
-for (let val of items){
-  let offer = val/10;
-  items[i]=items[i]-offer;
-  console.log(`value after offer = ${items[i]}`); 
-}
+// //In this query we use "for loop" bcz it give direct value but we ask about index tracking so we use only"for loop";
+// let items = [250,645,300,900,50];
+// let i=0;
+// for (let val of items){
+//   let offer = val/10;
+//   items[i]=items[i]-offer;
+//   console.log(`value after offer = ${items[i]}`); 
+// }
 
+// Mehods on Arrays Push() = add to end any word;
+// let Vagies = ["carrot","reddish","potato","tomato"];
+// Vegies.push("peas","ladyfinger");
+// console.log(Vegies);
+
+//Pop()
+// let Vegies = ["carrot","reddish","potato","tomato"];
+// console.log(Vegies);
+// let deletedVegies = Vegies.pop();
+// console.log(Vegies);
+// console.log(deletedVegies);
+
+//to string
+// let Vegies = ["carrot","reddish","potato","tomato"];
+// console.log(Vegies);
+// console.log(Vegies.toString());
+
+//Concat string= join one array to another array;
+// let marvelHeroes = ["Thor","spiderman","ironman"];
+// let dcHeroes = ["superman","shaktiman"];
+// let pakistaniHeroes = ["Quaid e Azam","Allama Iqbal"];
+// let heroes = marvelHeroes .concat(dcHeroes,pakistaniHeroes);
+// console.log(heroes);
+ 
+//unshift 
+// let marvelHeroes = ["Thor","spiderman","ironman"];
+// marvelHeroes.unshift("Faiza Majid");
+
+//shift = it return the delete value;
+// let marvelHeroes = ["Thor","spiderman","ironman"];
+// let values = marvelHeroes.shift();
+// console.log("deleted",values);
+
+//slice in  this end index is not included
+// let marvelHeroes = ["Thor","spiderman","ironman","antman","shaktiman"];
+// console.log(marvelHeroes);
+// console.log(marvelHeroes.slice(0,2));
+
+//splice  = (startdx,delcount,newEle) 
+let arr = [54,67,89,98,65];
+console.log(arr);
+arr.splice(2,2,101,102);
+
+// Practise Question
+
+//deleted from start
+let companies = ["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
+// companies.shift();
+
+// replace krna hai ola k sath;
+companies.splice(2,1,"ola");
+
+// at the end
+companies.push("Amazon");
